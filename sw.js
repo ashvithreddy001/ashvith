1045b5e312a344245ee0ac401612a8d0139faa82
ashvithreddy001 <113484901+ashvithreddy001@users.noreply.github.com>
2026-03-11 16:21:29 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
-const CACHE_NAME = 'jobmailer-v1';
-const ASSETS = ['./index.html'];
+const CACHE = 'jobmailer-v2';
+const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
